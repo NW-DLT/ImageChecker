d35248d9b4d7795cbcb68a18d7d8546215bcf8ab
NW_DLT <alexputil228@mail.ru>
2022-05-21 19:12:45 +0300

    fixCopyChek

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -101,7 +101,7 @@ namespace IT_StudioTestTask.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,ImageSrc,uploadTime,Disription")] Photos photos)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,ImageSrc,uploadTime,CopyCheck,Disription")] Photos photos)
         {
             _context.Update(photos);
             await _context.SaveChangesAsync();
